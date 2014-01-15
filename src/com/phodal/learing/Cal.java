7b4ca1b9efd50c4cb52ab7504ff51c274e9c479d
Phodal <gmszone@qq.com>
2014-01-15 16:37:34 +0800

    init

---
+++ src/com/phodal/learing/Cal.java
@@ -0,0 +1,11 @@
+package com.phodal.learing;
+
+/**
+ * Created by fdhuang on 1/15/14.
+ */
+public class Cal {
+
+    public int add(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/com/phodal/learing/Main.java
@@ -0,0 +1,12 @@
+package com.phodal.learing;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        Cal cal=new Cal();
+        int c=cal.add(1,2);
+        System.out.println("Hello,s");
+        System.out.println(c);
+    }
+}
