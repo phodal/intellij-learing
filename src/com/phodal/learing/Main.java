d5a54d022b670a7f7c6de9db2c1e79278cf5f152
Phodal <gmszone@qq.com>
2014-01-18 16:57:29 +0800

    [rename] [shift+f6] rename c to result_add,rename d to result_sub

--- src/com/phodal/learing/Main.java
+++ src/com/phodal/learing/Main.java
@@ -3,10 +3,10 @@ package com.phodal.learing;
 public class Main {
 
     public static void main(String[] args) {
-        int c=new Cal().add(1,2);
-        int d=new Cal2().sub(2,1);
+        int result_add=new Cal().add(1,2);
+        int result_sub=new Cal2().sub(2,1);
         System.out.println("Hello,s");
-        System.out.println(c);
-        System.out.println(d);
+        System.out.println(result_add);
+        System.out.println(result_sub);
     }
 }
